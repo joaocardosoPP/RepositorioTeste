7cd6fa13f36e057a8c42f18530fb62afb36dfa8d
Joao Cardoso <joaocardoso@prototipopadrao.com>
2011-11-24 11:34:00 +0000

    Teste 123

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+            Console.Read();
+        }
+    }
+}
